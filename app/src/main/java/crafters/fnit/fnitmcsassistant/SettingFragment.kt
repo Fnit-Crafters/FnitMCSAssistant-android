92139b408f9c2d985294243d9eaecb73d3cbfa0e
TaikiFnit <taikifnitspg@gmail.com>
2017-12-02 10:40:16 +0900

    complete push notification hock

--- app/src/main/java/crafters/fnit/fnitmcsassistant/SettingFragment.kt
+++ app/src/main/java/crafters/fnit/fnitmcsassistant/SettingFragment.kt
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.firebase.database.FirebaseDatabase
 
 
 /**
@@ -19,8 +21,18 @@ class SettingFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        val view = inflater!!.inflate(R.layout.fragment_setting, container, false)
+
+        val pushButton = view.findViewById<Button>(R.id.pushButton)
+        pushButton.setOnClickListener(object: View.OnClickListener {
+            override fun onClick(p0: View?) {
+                val ref = FirebaseDatabase.getInstance().reference
+                ref.child("pushNotifications").push().setValue(true)
+            }
+        })
+
         // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_setting, container, false)
+        return view
     }
 
     companion object {
